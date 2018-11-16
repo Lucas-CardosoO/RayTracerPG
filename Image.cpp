5243eb015ffbd32367017a902f733057de375035
Vitor Maia <vlma@cin.ufpe.br>
2018-11-16 12:10:33 -0200

    fix outro bug com posições da imagem

--- Image.cpp
+++ Image.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <iostream>
 
 Image::Image(int width, int height) {
     this->width = width;
@@ -9,23 +10,18 @@ Image::Image(int width, int height) {
     this->buffer.resize(this->width*this->height);
 }
 void Image::setPixel(int x, int y, const RGBColor& color) {
-    this->buffer[x*this->width+y] = color;
+    this->buffer[x*this->width + y] = color;
 }
 
 void Image::saveAsPBM(const std::string& filePath) const {
-    std::ofstream out(filePath+"\\out.pbm");
+    std::ofstream out(filePath+"\\cena.ppm");
+    out << "P3\n" << this->width << " " << this->height  << "\n255\n";
     for(int i = 0; i < this->height; i++) {
         for(int j = 0; j < this->width; j++) {
-            out << this->buffer[this->width*i+j].r << ' ' << this->buffer[this->width*i+j].g << ' ' << this->buffer[this->width*i+j].b << '\n';
+            // std::cout << "Putting px in (" << i <<", " << j<<")\nbuffer["<<i*this->width+j<<"] is the buffer pos\n";
+            out << this->buffer[i*this->width+j].r << ' ' << this->buffer[i*this->width+j].g << ' ' << this->buffer[i*this->width+j].b << '\n';
         }
     }
     out.close();
 }
 
-int Image::getWidth() {
-    return this->width;
-}
-
-int Image::getHeight() {
-    return this->height;
-}
--- Image.h
+++ Image.h
@@ -4,15 +4,11 @@
 #include <string>
 #include "RGBColor.h"
 
-class Image{
-private:
+class Image {
+public:
     int width, height;
     std::vector<RGBColor> buffer;
-
-public:
     Image(int width, int height);
-    int getWidth();
-    int getHeight();
     void setPixel(int x, int y, const RGBColor& color);
     void saveAsPBM(const std::string& filePath) const;
 };
