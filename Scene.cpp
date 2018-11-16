594566948fd0c675fc2bedfdc359f606c772fdae
Vitor Maia <vlma@cin.ufpe.br>
2018-11-16 13:38:18 -0200

    scene intersect method

--- Scene.cpp
+++ Scene.cpp
@@ -4,13 +4,26 @@
 #include "ObjectIntersection.h"
 #include "Ray.h"
 #include "Scene.h"
+#include <float.h>
 
 bool Scene::intersect(const Ray &r, ObjectIntersection* info) const {
-    return true;
+    ObjectIntersection *temp_info;
+    bool hit_anyone = false;
+    double t_max = DBL_MAX;
+    for(auto obj : objects) {
+        if(obj->intersect(r, temp_info)) {
+            hit_anyone = true;
+            if(temp_info->t < t_max) {
+                info = temp_info;
+                t_max = info->t;
+            }
+        }
+    }
+    return hit_anyone;
 }
 
 Vector3D Scene::trace(const Ray &r, int recursionLevel) const {
-    return Vector3D(0, 0, 0);
+    
 }
 
 void Scene::add(Object *object) {
